7264bceb86180a94bb5cc769feded490d50d4973
lhmaynard <lhm765@gmail.com>
2017-10-29 16:14:01 -0400

    added unit tests

--- src/test/java/project/SSQWorldOfSweetsTest.java
+++ src/test/java/project/SSQWorldOfSweetsTest.java
@@ -1,6 +1,7 @@
 /**
- * 
+ *
  */
+package project;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -8,9 +9,66 @@ import static org.junit.Assert.*;
 import project.SSQWorldOfSweets;
 
 public class SSQWorldOfSweetsTest {
-	
-	//@Test
-	//
-	
-}
 
+	//this test checks if the deck has the required 50 single cards
+	//User Story (Single and Double Cards)
+	@Test
+	public void testCountSingles(){
+		Deck d = new Deck();
+		int count =0;
+		while(!d.empty()){
+			if(d.drawCard()<5) count++;
+		}
+
+		assertTrue(count==50);
+
+	}
+
+	//this test checks if the deck has the required 10 double cards
+	//User Story (Single and Double Cards)
+	@Test
+	public void testCountDoubles(){
+		Deck d = new Deck();
+		int count =0;
+		while(!d.empty()){
+			if(d.drawCard()>4) count++;
+		}
+
+		assertTrue(count==10);
+
+	}
+
+	//This test checks that the ratio of single cards to doubles is 5:1
+	//User Story (Card Chances)
+	@Test
+	public void testSingleDoubleRatio(){
+		Deck d = new Deck();
+		int singles=0;
+		int doubles=0;
+		while(!d.empty()){
+			int c=d.drawCard();
+			if(c>4) doubles++;
+			else singles++;
+		}
+		int ratio = singles/doubles;
+		assertTrue(ratio==5);
+	}
+
+	//This test checks that there are more single cards than double cards
+	//User Story (Card Chances)
+	@Test
+	public void testMoreSingles(){
+		Deck d = new Deck();
+		int singles=0;
+		int doubles=0;
+		while(!d.empty()){
+			int c=d.drawCard();
+			if(c>4) doubles++;
+			else singles++;
+		}
+
+		assertTrue(singles>doubles);
+	}
+
+
+}
